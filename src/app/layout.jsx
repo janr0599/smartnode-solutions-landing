de93373948f54f7a0c13b5362686e206349e7d9a
janr0599 <javiernr0599@gmail.com>
2025-08-09 08:49:32 -0400

    Add: Google Analytics script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Toaster } from "sonner";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import the Script component
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="scroll-smooth">
+            <head>
+                {/* Google tag (gtag.js) */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-89ENM9L4J1"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-89ENM9L4J1');
+                    `}
+                </Script>
+            </head>
             <body className={inter.className}>
                 {children}
                 <Toaster />
